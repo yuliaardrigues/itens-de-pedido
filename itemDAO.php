0932b19ceb4be7219e6cc99b5df6df1416daed79
yulia <yuliarodrigues@aluno.unifapce.edu.br>
2023-06-08 00:44:22 -0300

    alteraçoes itemDAO

--- itemDAO.php
+++ itemDAO.php
@@ -3,8 +3,14 @@ require_once 'item.php';
 
 class itemDAO {
     public function create (Item $item) {
-        $sql = 'INSERT INTO item (nome, marca, tipo, quantidade, preco) VALUES (?,?,?,?,?)';
+        $sql = 'INSERT INTO item (idItem, nome, marca, tipo, quantidade, preco) VALUES (?,?,?,?,?,?)';
         $stmt = Conexao::getConn()->prepare($sql);
+        $stmt->bindValue(1, $item->getidItem());
+        $stmt->bindValue(2, $item->getNome());
+        $stmt->bindValue(3, $item->getMarca());
+        $stmt->bindValue(4, $item->getTipo());
+        $stmt->bindValue(5, $item->getQuantidade());
+        $stmt->bindValue(6, $item->getPreco());
     }
 }
 
