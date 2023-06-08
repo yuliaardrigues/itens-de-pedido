0bef62a46c773198aeab88b97bba1eb324c94b90
yulia <yuliarodrigues@aluno.unifapce.edu.br>
2023-06-07 21:34:51 -0300

    class item

---
+++ item.php
@@ -0,0 +1,131 @@
+<?php
+class item{
+    private $idItem;
+    private $nome;
+    private $marca;
+    private $tipo;
+    private $quantidade;
+    private $preco;
+
+    
+    /**
+     * Get the value of idItem
+     */ 
+    public function getIdItem()
+    {
+        return $this->idItem;
+    }
+
+    /**
+     * Set the value of idItem
+     *
+     * @return  self
+     */ 
+    public function setIdItem($idItem)
+    {
+        $this->idItem = $idItem;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */ 
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     *
+     * @return  self
+     */ 
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of marca
+     */ 
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * Set the value of marca
+     *
+     * @return  self
+     */ 
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of tipo
+     */ 
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    /**
+     * Set the value of tipo
+     *
+     * @return  self
+     */ 
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of quantidade
+     */ 
+    public function getQuantidade()
+    {
+        return $this->quantidade;
+    }
+
+    /**
+     * Set the value of quantidade
+     *
+     * @return  self
+     */ 
+    public function setQuantidade($quantidade)
+    {
+        $this->quantidade = $quantidade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of preco
+     */ 
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    /**
+     * Set the value of preco
+     *
+     * @return  self
+     */ 
+    public function setPreco($preco)
+    {
+        $this->preco = $preco;
+
+        return $this;
+    }
+}
+?>
